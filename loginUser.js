d8a289a2c8a5c2bb58992056e2e8607731c74b9a
Michael <123pie23@gmail.com>
2019-12-03 00:17:10 -0800

    removed unused codde and required nodeFns

--- loginUser.js
+++ loginUser.js
@@ -1,32 +1,12 @@
 const wrap = require("./wrap");
 const md5 = require('md5');
-
-const checkPassword = async (app, db, password) => {
-    password = md5(password);
-    let sql = "SELECT COUNT(*) AS rows FROM `accounts` WHERE `password` = ?";
-    db.connect(() => {
-        db.query(sql, password, (err, data) => {
-            if (!err) {
-                let newPassword = false;
-                let rows = data[0].rows;
-                if (!rows) {
-                    newPassword = true;
-                }
-                console.log('newPassword:' + newPassword);
-                return newPassword;
-            } else {
-                console.log(err);
-                return false;
-            }
-        })
-    })
-}
+const nodeFns = require('./nodeFns');
 
 module.exports = (app, db) => {
     app.post('/loginUser', wrap(async (req, res) => {
         req.body.password = md5(req.body.password);
         let {email, password} = req.body;
-        let newPassword = await checkPassword(app, db, password);
+        let newPassword = await nodeFns.checkPassword(app, db, password);
         console.log('newPassword2:' + newPassword);
         if (!newPassword) {
             let output = {
@@ -36,20 +16,6 @@ module.exports = (app, db) => {
             res.send(output);
             return;
         }
-
-        //// routes/video.js
-        // const wrap = require("../middleware/wrap");
-        // router.get("/", wrap(async (req, res, next) => {
-        //   let videos = await media.getAll({
-        //     lang: req.getLocale(),
-        //     type: 1
-        //   });
-        //
-        //   res.render("videos", {
-        //     videos
-        //   });
-        // }));
-
         let sql = "SELECT `token` FROM `accounts` WHERE `email` = ? AND `password` = ?";
         db.connect(() => {
             db.query(sql, [email, password], (err, data) => {
@@ -71,5 +37,5 @@ module.exports = (app, db) => {
             })
         });
     }));
-}
+};
 
