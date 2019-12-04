81e70583fd45ca2d5f1a423669f9c6618e150bd4
Michael <123pie23@gmail.com>
2019-12-04 00:34:49 -0800

    old code update

--- nodeFns.js
+++ nodeFns.js
@@ -1,25 +1,23 @@
 const md5 = require('md5');
 
 module.exports.checkPassword = checkPassword = async (app, db, password) => {
-    app.post('/checkPassword', (req, res) => {
-        password = md5(password);
-        let sql = "SELECT COUNT(*) AS rows FROM `accounts` WHERE `password` = ?";
-        db.connect(() => {
-            db.query(sql, password, (err, data) => {
-                if (!err) {
-                    let newPassword = false;
-                    let rows = data[0].rows;
-                    if (!rows) {
-                        newPassword = true;
-                    }
-                    console.log('newPassword:' + newPassword);
-                    res.send(newPassword)
-                } else {
-                    console.log(err);
+    password = md5(password);
+    let sql = "SELECT COUNT(*) AS rows FROM `accounts` WHERE `password` = ?";
+    db.connect(() => {
+        db.query(sql, password, (err, data) => {
+            if (!err) {
+                let newPassword = false;
+                let rows = data[0].rows;
+                if (!rows) {
+                    newPassword = true;
                 }
-            })
-        });
-    })
+                console.log('newPassword:' + newPassword);
+                return newPassword
+            } else {
+                console.log(err);
+            }
+        })
+    });
 };
 
 
